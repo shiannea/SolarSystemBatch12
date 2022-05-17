30dd33c9c147850e83bd1accb6eb23de9a92f737
shiannea <shianne.ali99@gmail.com>
2022-05-17 19:33:01 -0400

    Feature Sun added Successfully

---
+++ src/com/syntax/FeatureSun.java
@@ -0,0 +1,8 @@
+package com.syntax;
+
+public class FeatureSun {
+    String color;
+    String radius;
+    String heat;
+
+}
--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -9,6 +9,11 @@ public class Main {
         factorX.moon="4";
         factorX.star="10000";
         factorX.planet="2";
+
+        FeatureSun xFac=new FeatureSun();
+        xFac.color="green";
+        xFac.radius="100000000km";
+        xFac.heat="2992777266545789kj";
         
     }
 }
